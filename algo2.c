700d96ae766c931023e3967cd279486a6c8a65db
Mohammed El Haddioui <mel-hadd@c4r6p6.1337.ma>
2024-02-05 18:36:50 +0100

    push

--- algo2.c
+++ algo2.c
@@ -6,20 +6,20 @@
 /*   By: mel-hadd <mel-hadd@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/03 14:24:49 by mel-hadd          #+#    #+#             */
-/*   Updated: 2024/02/05 17:34:25 by mel-hadd         ###   ########.fr       */
+/*   Updated: 2024/02/05 18:35:39 by mel-hadd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
-int	get_index(t_list *stack_a)
+int	get_index(int nb, t_list *stack_a)
 {
 	int	index;
 
 	index = 0;
 	while (stack_a)
 	{
-		if (stack_a->content == min(stack_a)) 
-			break ;
+		if (stack_a->content == nb)
+			break;
 		stack_a = stack_a->next;
 		index++;
 	}
@@ -57,15 +57,48 @@ int *ft_copy(t_list *stack_a, int len)
 		i++;
 	}
 	return arr;
-
 }
-
+int get_i(int *arr, int len, int nb)
+{
+	int i;
+	i = 0;
+	while (i < len)
+	{
+		if (nb == arr[i])
+			break;
+		i++;
+	}
+	return i;	
+}
+void ft_display(t_list *stack_a)
+{
+	while(stack_a)
+	{
+		printf("%d\n" ,stack_a->content);
+		stack_a = stack_a->next;
+	}
+}
 void	sort_100(t_list **stack_a, t_list **stack_b, int c)
 {
-	int *arr = malloc(sizeof(int) * c);
+	int *arr;
 	arr = ft_copy(*stack_a, c);
-	int index_a;
-	index_a = get_index(*stack_a);
-	printf("%d\n",index_a);
-	(void)stack_b;
+	int index_arr;
+	// int index_a;
+	int i = 0;
+	while (i < c)
+	{
+		
+		index_arr = get_i(arr,c,(*stack_a)->content);
+		if (index_arr <= i)
+	{
+			pb(stack_a,stack_b);
+		
+		i++;
+	}
+		else
+			ra(stack_a,0);
+	}
+	ft_display(*stack_b);
+	exit(0);
+
 }
